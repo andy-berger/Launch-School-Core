0ca4a8421fc3c229e103e0c0cf6f4abb8636dea1
Andy Berger <inocid@gmail.com>
2025-03-01 16:04:37 +0100

    Adding bonus features to calculator

--- 01_PY101/lesson_2/calculator.py
+++ 01_PY101/lesson_2/calculator.py
@@ -1,46 +1,74 @@
-def prompt(message):
+import json
+
+LANG = "en"
+
+# Open the JSON file for reading
+with open('calculator_messages.json', 'r') as file:
+    messages = json.load(file)
+
+def get_message(message, lang=LANG):
+    return messages[lang][message]
+
+def prompt(key):
+    message = get_message(key, LANG)
     print(f"==> {message}")
 
 def invalid_number(number_str):
     try:
-        int(number_str)
+        float(number_str)
     except ValueError:
         return True
 
     return False
 
-prompt("Welcome to Calculator!")
-
-prompt("What's the first number?")
-number1 = input()
-
-while invalid_number(number1):
-    prompt("Hmm... that doesn't look like a valid number.")
+def perform_calculation():
+    prompt("first_num")
     number1 = input()
 
-prompt("What's the second number?")
-number2 = input()
+    while invalid_number(number1):
+        prompt("invalid_num")
+        number1 = input()
 
-while invalid_number(number2):
-    prompt("Hmm... that doesn' look like a valid number.")
+    prompt("second_num")
     number2 = input()
 
-prompt("What operation would you like to perform?\n"
-       "1) Add 2) Subtract 3) Multiply 4) Divide")
-operation = input()
+    while invalid_number(number2):
+        prompt("invalid_num")
+        number2 = input()
 
-while operation not in ['1', '2', '3', '4']:
-    prompt("You must choose 1, 2, 3, or 4")
+    prompt("operation")
     operation = input()
 
-match operation:
-    case "1":
-        output = int(number1) + int(number2)
-    case "2":
-        output = int(number1) - int(number2)
-    case "3":
-        output = int(number1) * int(number2)
-    case "4":
-        output = int(number1) / int(number2)
+    while operation not in ["1", "2", "3", "4"]:
+        prompt("operation_validation")
+        operation = input()
 
-prompt(f"The result is: {output}")
+    match operation:
+        case "1":
+            output = float(number1) + float(number2)
+        case "2":
+            output = float(number1) - float(number2)
+        case "3":
+            output = float(number1) * float(number2)
+        case "4":
+            output = float(number1) / float(number2)
+
+    print(f"{messages[LANG]["result"].format(output=output)}")
+    perform_another_calculation()
+
+def perform_another_calculation():
+    prompt("another_calc")
+    another_calc = input()
+
+    while another_calc not in [messages[LANG]["positive"],
+                               messages[LANG]["negative"],
+                               messages[LANG]["positive"].upper(),
+                               messages[LANG]["negative"].upper()]:
+        prompt("another_calc")
+        another_calc = input()
+
+    if another_calc.casefold() == messages[LANG]["positive"]:
+        perform_calculation()
+
+prompt("welcome")
+perform_calculation()
