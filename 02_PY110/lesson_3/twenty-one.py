24c8054a74b21c32696eae7d619541fce0f8b8b9
Andy Berger <inocid@gmail.com>
2025-05-16 15:12:39 +0200

    Twenty-One game

---
+++ 02_PY110/lesson_3/twenty-one.py
@@ -0,0 +1,139 @@
+import random
+import os
+
+DECK_SUITS = ["H", "D", "C", "S"]
+DECK_NUMERIC_VALUES = ["2", "3", "4", "5", "6", "7", "8", "9", "10"]
+DECK_VALUES = DECK_NUMERIC_VALUES + ["J", "Q", "K", "A"]
+DECK_TERMS = {"J": "Jack", "Q": "Queen", "K": "King", "A": "Ace"}
+
+def shuffle_deck(deck):
+    random.shuffle(deck)
+
+def initialize_deck():
+    deck = []
+    for suit in DECK_SUITS:
+        for value in DECK_VALUES:
+            deck.append([suit, value])
+
+    shuffle_deck(deck)
+    return deck
+
+def deal_cards(deck):
+    return deck.pop()
+
+def deal_initial_cards(deck):
+    return [deal_cards(deck), deal_cards(deck)]
+
+def translate_card(card):
+    if card[1] in DECK_NUMERIC_VALUES:
+        return card[1]
+    else:
+        return DECK_TERMS[card[1]]
+
+def prompt(message):
+    print(f"=> {message}")
+
+def get_validated_input(prompt_message, valid_options):
+    prompt(f"{prompt_message} ")
+    while True:
+        answer = input().lower().strip()
+        if answer in valid_options:
+            return answer
+        prompt(f"Please enter one of: {', '.join(valid_options)}")
+
+def player_turn():
+    answer = get_validated_input("Please choose: hit or stay?", ["hit", "stay", "h", "s"])
+    return answer
+
+def total(cards):
+    # cards = [['H', '3'], ['S', 'Q'], ... ]
+    values = [card[1] for card in cards]
+
+    sum_val = 0
+    for value in values:
+        if value == "A":
+            sum_val += 11
+        elif value in ['J', 'Q', 'K']:
+            sum_val += 10
+        else:
+            sum_val += int(value)
+
+    # correct for Aces
+    aces = values.count("A")
+    while sum_val > 21 and aces:
+        sum_val -= 10
+        aces -= 1
+
+    return sum_val
+
+def busted(cards):
+    return total(cards) > 21
+
+def determine_winner(player_hand, dealer_hand):
+    player_score = total(player_hand)
+    dealer_score = total(dealer_hand)
+
+    score = f"Player score: {player_score} | Dealer score: {dealer_score}"
+
+    if busted(player_hand):
+        return "Dealer", "Player busted", score
+    elif busted(dealer_hand):
+        return "Player", "Dealer busted", score
+
+    if player_score == dealer_score:
+        return "no one", "tie", score
+    elif player_score > dealer_score:
+            return "Player", "Player won", score
+    elif player_score < dealer_score:
+            return "Dealer", "Dealer won", score
+
+def announce_winner(player_hand, dealer_hand):
+    winner, reason, score = determine_winner(player_hand, dealer_hand)
+
+    if "busted" in reason:
+        print(f"{reason}, {winner.lower()} won! {score}")
+    elif reason == "tie":
+        print(f"It's a {reason}! {score}")
+    else:
+        print(f"{reason}! {score}")
+
+def hit(deck, cards):
+    return cards + [deal_cards(deck)]
+
+def play_twenty_one():
+    deck = initialize_deck()
+    player_hand = deal_initial_cards(deck)
+    dealer_hand = deal_initial_cards(deck)
+
+    prompt(f"Dealer has: {translate_card(dealer_hand[0])} and unknown card")
+    prompt(f"You have:   {translate_card(player_hand[0])} and {translate_card(player_hand[1])}")
+    while True:
+        player_decision = player_turn()
+        if player_decision in ["hit", "h"]:
+            player_hand = hit(deck, player_hand)
+            prompt(f"You chose to hit! Your cards are now: {', '.join(translate_card(card) for card in player_hand)}")
+            prompt(f"Your total is now: {total(player_hand)}")
+            if busted(player_hand):
+                break
+        elif player_decision in ["stay", "s"]:
+            prompt("You chose to stay!")
+            break
+
+    if not busted(player_hand):
+        prompt(f"Dealer reveals: {translate_card(dealer_hand[0])} and {translate_card(dealer_hand[1])}")
+        prompt(f"Dealer's total is: {total(dealer_hand)}")
+
+        while total(dealer_hand) < 17:
+            dealer_hand = hit(deck, dealer_hand)
+            prompt(f"Dealer hits! Dealer's cards are now: {', '.join(translate_card(card) for card in dealer_hand)}")
+            prompt(f"Dealer's total is now: {total(dealer_hand)}")
+
+    announce_winner(player_hand, dealer_hand)
+
+while True:
+    os.system("clear")
+    play_twenty_one()
+    another_round = get_validated_input("play again? (y/n)", ["y", "n"])
+    if another_round == "n":
+        prompt("Thank you for playing Twenty-One!")
+        break
