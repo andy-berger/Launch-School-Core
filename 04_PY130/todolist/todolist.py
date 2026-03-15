72d77ebfbfea1364cb15ec24e6161cb06a2fe2a3
Andy Berger <inocid@gmail.com>
2026-03-15 11:33:24 +0100

    Added test suite for TodoList

--- 04_PY130/todolist/todolist.py
+++ 04_PY130/todolist/todolist.py
@@ -1,32 +1,4 @@
-class Todo:
-    DONE_MARKER = 'X'
-    UNDONE_MARKER = ' '
-
-    def __init__(self, title):
-        self._title = title
-        self._done = False
-
-    @property
-    def title(self):
-        return self._title
-
-    @property
-    def done(self):
-        return self._done
-
-    @done.setter
-    def done(self, done):
-        self._done = done
-
-    def __str__(self):
-        marker = Todo.DONE_MARKER if self.done else Todo.UNDONE_MARKER
-        return f"[{marker}] {self.title}"
-
-    def __eq__(self, other):
-        if isinstance(other, Todo):
-            return self.title == other.title and self.done == other.done
-
-        return NotImplemented
+from todo import Todo
 
 class TodoList:
     def __init__(self, title):
@@ -44,7 +16,7 @@ class TodoList:
         self._todos.append(todo)
 
     def __str__(self):
-        output_lines = [f"----- {self.title} ------"]
+        output_lines = [f"----- {self.title} -----"]
         output_lines += [str(todo) for todo in self._todos]
         return "\n".join(output_lines)
 
